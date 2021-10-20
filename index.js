0cd3af2954046db1f7f99ba92efb80459684b0ad
jacob <jisraelarguelles@gmail.com>
2021-10-20 12:22:26 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const cors = require("cors");
+const informacion = require("./informacion");
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+//Metodos GET
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hola, te saluda Jacob</h1>");
+});
+app.get("/informacion", (req, res) => {
+  res.json(informacion);
+});
+
+app.get("/informacion/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const resultado_peticion = informacion.find((info) => info.id === id);
+  if (resultado_peticion) {
+    res.json(resultado_peticion);
+  } else {
+    res.status(404).end();
+  }
+});
+
+//Metodo DELETE
+
+app.delete("/informacion/:id", (req, res) => {
+  const id = Number(req.params.id);
+  informacion = informacion.filter((info) => info.id !== id);
+  res.status(400).json({ error: "falta informacion requerida" });
+});
+
+//Metodo POST
+
+app.post("/informacion", (req, res) => {
+  const infor = req.body;
+  if (!infor || !infor.content) {
+    return res.status(400).end();
+  }
+  const ID = informacion.map((info) => info.id);
+  const maxi_ID = Math.max(...id);
+  const newinfo = {
+    id: maxi_ID + 1,
+    content: infor.content,
+  };
+  res.status(201).json(infor);
+});
+
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log("el servidor se esta ejecutando en el puerto:" + PORT);
+});
---
+++ informacion.js
@@ -0,0 +1,25 @@
+let informacion = [
+  {
+    id: 1,
+    nombre: "Jacob",
+    apellido: "Arguelles",
+    edad: "22 años",
+    celular: ["04146698579", "02682527046"],
+  },
+  {
+    id: 2,
+    nombre: "Adriana",
+    apellido: "Gutierrez",
+    edad: "18 años",
+    celular: ["04146698579", "02682527046"],
+  },
+  {
+    id: 3,
+    nombre: "Pedro",
+    apellido: "gutierrez",
+    edad: "48 años",
+    celular: ["04146698579", "02682527046"],
+  },
+];
+
+module.exports = informacion;
