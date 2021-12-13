de37658bc18191bac660e78ddc86d019fce5d6c6
jacob arguelles <91341876+jacob-arguelles@users.noreply.github.com>
2021-12-12 23:26:24 -0400

    add json2xml (require)

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const notFound = require("./middleware/notFound");
 const handleError = require("./middleware/handleError");
 const app = express();
 
+const xml = require('xml')
+
 app.use(cors());
 app.use(express.json());
 
